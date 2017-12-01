96778219f5e55dec06b4ada4d582f910ecdff0ce
WEB\a-jchan <jwchan1993@gmail.com>
2017-12-01 13:52:57 +0000

    Added styles to details page, added a details page js file, altered background across all pages

---
+++ 1994-child-page_files/1994-details-page-js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('td:eq(5)').remove();
+    $('td:eq(5)').remove();
+    $('td:eq(3)').remove();
+    $('td:eq(4)').remove();
+    $('h1').insertBefore('td:eq(8)');
+    $('h1').html('Accessions to Repositories');
+    // $('a:eq(23)').html('<a class="ghost-button" href="#">Choose a different year</a>');
+});
--- landing_files/tna-accessions-js.js
+++ landing_files/tna-accessions-js.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	
-	$('h1').insertBefore('h2:eq(0)');
 
-	var h1Mod = function(){
-		if($('h1 img').is('img')){
-			$('h1').html('Accessions to Repositories');
-		}
-	}
-	h1Mod();
+    $('h1').insertBefore('h2:eq(0)');
+
+    var h1Mod = function(){
+        if($('h1 img').is('img')){
+            $('h1').html('Accessions to Repositories');
+        }
+    }
+    h1Mod();
 });
